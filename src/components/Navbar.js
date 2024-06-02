4e5ebfdbdb87ecbb994e7409bb6da0a89eb3619c
Shruthi G S <git21cs173-t@students.git.edu>
2024-06-02 16:49:00 +0530

    responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Nav>
-      <NavList>
+      <Logo href="#home">My Portfolio</Logo>
+      <Hamburger onClick={toggleNavbar}>
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+      <NavList isOpen={isOpen}>
         <NavItem><a href="#home">Home</a></NavItem>
         <NavItem><a href="#about">About</a></NavItem>
         <NavItem><a href="#projects">Projects</a></NavItem>
@@ -26,6 +38,40 @@ const Nav = styled.nav`
   position: sticky;
   top: 0;
   z-index: 1000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.a`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-left: 1em;
+  color: #000;
+  text-decoration: none;
+
+  &:hover {
+    color: #ff69b4;
+  }
+`;
+
+const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  margin-right: 1em;
+
+  span {
+    height: 3px;
+    width: 25px;
+    background: #000;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
 `;
 
 const NavList = styled.ul`
@@ -33,17 +79,35 @@ const NavList = styled.ul`
   display: flex;
   justify-content: center;
   padding: 0;
+  margin: 0;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+    position: absolute;
+    top: 60px; /* Adjust according to your Navbar height */
+    left: ${({ isOpen }) => (isOpen ? '0' : '-100%')};
+    background: #ffe4e1;
+    transition: left 0.3s ease-in-out;
+  }
 `;
 
 const NavItem = styled.li`
   margin: 0 15px;
+
   a {
     color: #000;
     text-decoration: none;
     font-weight: 700;
     transition: color 0.3s ease;
   }
+
   a:hover {
     color: #ff69b4;
+  }
+
+  @media (max-width: 768px) {
+    margin: 15px 0;
+    text-align: center;
   }
 `;
